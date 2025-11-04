6401d8984867fe8d4be9f04fd40d1f5713c8fb67
CattiesWorld <219463308+cattiesworld@users.noreply.github.com>
2025-11-05 00:54:47 +0100

    YES THAT MIGHT HAVE TAKEN MULITPLE FUCKING HOURS!!!!!

--- script/codeblocks.js
+++ script/codeblocks.js
@@ -2,21 +2,25 @@ const blocks = {}
 
 defineBlock('test block', 'transform.shift(20, 20)', 'sex')
 
-
-(() => {
-    const codeblocks = document.getElementById('codeblocks')
-    const b = Object.keys(blocks)
-    b.forEach(block => {
-        const el = document.createElement('p')
-        el.textContent = block
-        codeblocks.appendChild(el)
-    })
-})()
-
-
-function defineBlock(text, call, id) {
+function defineBlock(t, c, id) {
     blocks[id] = {
-        text: text,
-        call: call
+        text: t,
+        call: c
     }
 }
+
+function generateCodeblocks() {
+    const codeblocks = document.getElementById('codeblocks')
+    const b = Object.keys(blocks)
+    let i = 0
+    b.forEach(block => {
+        const el = document.createElement('p')
+        el.textContent = blocks[block].text
+        el.className = 'codeblock'
+        codeblocks.appendChild(el)
+        i++
+    })    
+}
+
+generateCodeblocks()
+
